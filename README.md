fd9f341f9007a9bd09f4be86097c2bb39c53858f
Karolina Stawicka <Karolina@Karolinas-MBP.fritz.box>
2022-01-24 17:48:35 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# My data science environment
+
+This is a helper read me file with information what is in this project!
---
+++ test.py
@@ -0,0 +1,12 @@
+from pickle import BINSTRING
+import pandas as pd
+
+print('HELLO WORLD')
+
+d = {}
+d["ap"] = "apples" 
+d["bi"] = "bins" 
+d["do"] = "dods" 
+d["el"] = "elephants"
+
+print(d)
